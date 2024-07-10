1c996d99a340b5cde54c2a6b901a432f09a62d4f
Andres Garcia <andres.garcia@databricks.com>
2024-07-10 10:30:50 -0600

    Adding Github actions

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,37 @@
-from cicd_w_dabs_ga_demo.main import get_taxis, get_spark
+# Databricks notebook source
+# MAGIC %pip install -U nutter
+
+# COMMAND ----------
+
+# MAGIC %load_ext autoreload
+# MAGIC %autoreload 2
+# MAGIC
+
+# COMMAND ----------
+
+from runtime.nutterfixture import NutterFixture, tag
+from src.cicd_w_dabs_ado_demo.main import get_taxis, get_spark
 
 
-def test_main():
-    taxis = get_taxis(get_spark())
-    assert taxis.count() > 5
+class TestTaxisData(NutterFixture):
+    def __init__(self):
+        NutterFixture.__init__(self)
+
+    def assertion_test_main(self):
+        taxis = get_taxis(get_spark())
+        assert taxis.count() > 5, "Results returned less than 5 Records"
+
+
+# COMMAND ----------
+
+result = TestTaxisData().execute_tests()
+print(result.to_string())
+is_job = (
+    dbutils.notebook.entry_point.getDbutils()
+    .notebook()
+    .getContext()
+    .currentRunId()
+    .isDefined()
+)
+if is_job:
+    result.exit(dbutils)
